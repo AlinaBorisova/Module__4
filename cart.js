91bd6160a15525180bb9759d2d3b4ffbfebd1da1
Алина Борисова <b-alina1999.borisova@yandex.ru>
2023-06-07 18:41:39 +0300

    For check

--- cart.js
+++ cart.js
@@ -14,7 +14,6 @@ Cart.prototype.calculateGoodsPrice = function () {
             sum += item.goods.price;
         }
     });
-    console.log('this.goods', this.goods)
     this.totalPrice = sum;
 };
 
@@ -47,6 +46,7 @@ Cart.prototype.print = function () {
 
 const Goods = function(options) {
     Cart.apply(this, arguments);
+    this.count = 1;
 };
 const FoodGoods = function (options) {
     Goods.apply(this, arguments);
@@ -73,96 +73,4 @@ modelCart.addGoods(food, clothes, tech);
 console.log(modelCart);
 
 modelCart.print();
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Cart = {
-//     goods: [],
-//     totalPrice: 0,
-//     count: 0,
-//
-//     getTotalPrice: function() {
-//         return this.totalPrice;
-//     },
-//     addGoods: function (title, price, count = 1) {
-//         Cart.goods.push({
-//             title: title,
-//             price: price,
-//             count: count,
-//         });
-//         Cart.increaseCount(count);
-//         Cart.calculateGoodsPrice();
-//     },
-//     increaseCount: function(n) {
-//         this.count += n;
-//     },
-//     calculateGoodsPrice: function () {
-//         let sum = 0;
-//         this.goods.forEach(item => {
-//             sum += item.price * item.count
-//         })
-//         this.totalPrice = sum;
-//     },
-//     clear: function() {
-//         Cart.goods = [];
-//         this.totalPrice = 0;
-//         this.count = 0;
-//     },
-//     prints: function() {
-//         const cartStr = JSON.stringify(Cart.goods);
-//         console.log('totalPrice =', this.getTotalPrice());
-//     },
-// }
-
-// let fruits = new Cart.addGoods('Apple', 200);
-// let veg = new Cart.addGoods('Tomato', 250);
-// let meat = new Cart.addGoods('Pork', 800);
-// cart.clear();
-
-// Goods.prototype.FoodGoods = function (kkal) {
-//     return this.kkal = kkal;
-// };
-//
-// Goods.prototype.СlothingGoods = function (material) {
-//     return this.material = material;
-// };
-//
-// Goods.prototype.TechnicsGoods = function (typeTech) {
-//     return this.typeTech = typeTech;
-// };
-
-//
-// const Animal = function (options) {
-//     this.name = options.name;
-//     this.color = options.color;
-// };
-//
-// Animal.prototype.voice = function () {
-//     console.log('Say', this.name)
-// }
-//
-// const Cat = function (options) {
-//     Animal.apply(this, arguments)
-//     this.hasTail = options.hasTail
-//     this.type = 'cat'
-// }
-//
-// Cat.prototype = Object.create(Animal.prototype);
-// Cat.prototype.constructor = Cat
-//
-// const mur = new Cat({name: 'caty', color: 'red', hasTail: true})
-// console.log(mur)
-//
-// const mura = new Cat({name: 'catys', color: 'red', hasTail: true})
-// console.log(mura)
 
